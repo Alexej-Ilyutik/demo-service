21534fbb6f089344e4db13a106439f7b482a5423
Alexej Ilyutik <alexej.ilyutik@gmail.com>
2022-07-18 00:53:08 +0300

    finish task

--- demo-service/script.js
+++ demo-service/script.js
@@ -1,13 +1,14 @@
 const BTN_RESULT = document.querySelector('.input__result');
-const BTN_CLEAR = document.querySelector('.input__result');
-const BTN_REFRESH = document.querySelector('.input__result');
+const BTN_CLEAR = document.querySelector('.clear');
+const BTN_REFRESH = document.querySelector('.refresh');
 const MAIN_RESULT = document.querySelector('.result');
 const RESULT = document.querySelector('.result__area');
 const TEXTAREA = document.querySelector('.input__textarea');
 const INPUT_ALL = document.querySelectorAll('.checkbox__input');
 const RADIO_ALL = document.querySelectorAll('.radio__input');
 const SELECT = document.querySelector('.option__select');
-
+const AREA_TEXT =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
 function showResult() {
   let checkboxStr = checkInput(INPUT_ALL);
@@ -23,6 +24,8 @@ function showResult() {
   Option: ${SELECT.value};
   `;
 
+  localStorage.setItem('TEXTAREA', TEXTAREA.value);
+  localStorage.setItem('SELECT', SELECT.value);
 }
 
 function checkInput(input) {
@@ -30,14 +33,47 @@ function checkInput(input) {
   input.forEach((el) => {
     if (el.checked) {
       str = str.concat(`${el.dataset.value}, `);
+      localStorage.setItem(`${el.id}`, (el.checked = true));
+    } else {
+      localStorage.setItem(`${el.id}`, (el.checked = false));
     }
   });
-  if(!str){
+  if (!str) {
     str = 'Not selected!..';
   }
   return str.slice(0, -2);
 }
 
+function clearArea() {
+  TEXTAREA.value = '';
+}
 
+function refreshArea() {
+  TEXTAREA.value = `"${AREA_TEXT}"`;
+}
 
 BTN_RESULT.onclick = showResult;
+BTN_CLEAR.onclick = clearArea;
+BTN_REFRESH.onclick = refreshArea;
+
+let storageArea = localStorage.getItem('TEXTAREA');
+let storageSelect = localStorage.getItem('SELECT');
+const inputs = document.querySelectorAll('input');
+
+window.onload = function () {
+  if (storageArea) {
+    TEXTAREA.value = `${storageArea}`;
+  } else {
+    refreshArea();
+  }
+  if (storageSelect) {
+    SELECT.value = `${storageSelect}`;
+  }
+
+  inputs.forEach((el) => {
+    if (localStorage.getItem(`${el.id}`) === 'true') {
+      el.checked = true;
+    }
+  });
+
+};
